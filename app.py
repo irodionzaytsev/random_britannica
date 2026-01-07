02f53c4297677e9a506c5ca9398cf3a1ccc7191a
Rodion <irodionzaytsev@gmail.com>
2026-01-07 08:16:03 -0800

    Initial commit: working website

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import string
+import random
+import requests
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+def get_base_url():
+    """returns the url of britannica website"""
+    return "https://www.britannica.com"
+
+def query_random_category():
+    """This function returns the html query of a random category on the britannica.com website."""
+    categories = ["0-9"] + list(string.ascii_lowercase)
+    choice = random.choice(categories)
+    return requests.get(get_base_url() + "/sitemap/"+ choice)
+
+def query_random_topic(category):
+    soup = BeautifulSoup(category.content, "html.parser")
+    links_section = soup.find("ul",class_="list-unstyled md-az-browse-content")
+    links = links_section.find_all("li")
+    random_link = random.choice(links).find("a", href=True)['href']
+    return requests.get(get_base_url() + random_link)
+
+def get_random_article_link(topic):
+    soup = BeautifulSoup(topic.content, "html.parser")
+    links_section = soup.find("ul", class_="list-unstyled md-az-browse-content")
+    links = links_section.find_all("li")
+    random_link = random.choice(links).find("a", href=True)['href']
+    link = get_base_url() + random_link
+
+    return "<a href="+link+">"+random_link[1:]+"</a>"
+@app.route('/')
+def hello_world():  # put application's code here
+    category = query_random_category()
+    topic = query_random_topic(category)
+    article = get_random_article_link(topic)
+    return article
+
+if __name__ == '__main__':
+    app.run()
---
+++ logic.md
@@ -0,0 +1,9 @@
+This app performs 3 steps:
+### Step 1
+Get a random category, e.g.get a random category, e.g. "https://www.britannica.com/sitemap/c". This step is simple, as we just need to choose a random letter a-z.
+
+### Step 2
+Query how many links there are in that category and choose one of them, e.g. "https://www.britannica.com/sitemap/c/190".
+
+### Step 3
+On that page get the article link and redirect there, e.g. "https://www.britannica.com/topic/The-Coming-of-Post-Industrial-Society-A-Venture-in-Social-Forecasting" (that's the first link in my example)
